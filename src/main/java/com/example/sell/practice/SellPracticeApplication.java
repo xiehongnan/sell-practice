e2652484411480941947084ed3a49bdd6b117d3a
xiehongnan <xiehn007@sina.com>
2019-09-16 11:11:40 +0800

    commit

---
+++ src/main/java/com/example/sell/practice/SellPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sell.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellPracticeApplication.class, args);
+	}
+
+}
